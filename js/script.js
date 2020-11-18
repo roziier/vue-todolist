230b6675fe4230454fd30cf1e64d62f04ea4ba41
roziier <rikirosa93@gmail.com>
2020-11-18 14:01:22 +0100

    base

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Features principali:
+// un input permette all’utente di scrivere una “cosa da fare”;
+// un bottone permette di aggiungere quella cosa alla lista di cose da fare in pagina, come promemoria;
+// una volta che l’utente ha ipoteticamente svolto l’attività, grazie ad un bottone (X) può eliminare quella determinata cosa dalla lista.
+
+// STRUTTURA vue
+var app = new Vue({
+  el: "#root",
+});
